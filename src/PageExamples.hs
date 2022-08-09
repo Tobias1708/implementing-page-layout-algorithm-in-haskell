15ea6a2185de298a1331be663318f767aaa10ff9
Tobias Löser <92975796+Tobias1708@users.noreply.github.com>
2022-08-09 09:04:49 +0200

    minor changes

--- src/PageExamples.hs
+++ src/PageExamples.hs
@@ -21,13 +21,13 @@ defaultLines = [ fromIntegral $ points (fromIntegral $ fst $ computeBoxSize' def
 
 -- experimental
 defaultPageHeights :: [Int]
-defaultPageHeights = concat $ replicate 8 [fromIntegral $ points (fromIntegral $ snd $ computeBoxSize' defaultMargins)]
+defaultPageHeights = concat $ replicate 8 [fromIntegral $ points (fromIntegral $ snd (computeBoxSize' defaultMargins) + 5 * getBotMargin defaultMargins)]
 
 defaultFont :: IO Font
 defaultFont = loadFont (Right defaultFontSize) "C:/texlive/2021/texmf-dist/fonts/tfm/public/cm/cmr10.tfm"
 
 defaultFontSize :: Word32
-defaultFontSize = 65536 * 10
+defaultFontSize = 65536 * 10 
 
 defaultInterlineFactor :: Float
 defaultInterlineFactor = 1.2
@@ -44,7 +44,7 @@ defaultLinebreaker items lines' parameters' font fileName = do
     node = simpleDviNodeOutput font defaultInterlineFactor result
   singlePageDocument node $ "./files/" ++ fileName
 
-
+-- first attempt to make multi-page documents, by building them by hand
 multiPageLinebreaker :: [Item Char] -> [Int] -> LB.Parameters -> Font -> String -> IO()
 multiPageLinebreaker items lines' parameters' font fileName = do
   let
@@ -63,7 +63,7 @@ multiPageLinebreaker items lines' parameters' font fileName = do
     pageContent font ilf margs p = shiftPage (points (fromIntegral $ PB.getLeftMargin margs), points (fromIntegral $ PB.getTopMargin margs)) $
                                                         renderNode (simpleDviNodeOutput font ilf (getLines p))
 
-
+-- creates multi-page documents out of the result of the simple page-breaking algorithm
 multiPageLinebreaker' :: [Item Char] -> [Int] -> LB.Parameters -> Font -> String -> IO()
 multiPageLinebreaker' items lines' parameters' font fileName = do
   let
@@ -84,7 +84,7 @@ multiPageLinebreaker' items lines' parameters' font fileName = do
                                     shiftPage (points $ fromIntegral 226, points $ fromIntegral 30) $ renderNode $ simpleDviNodeOutput font ilf [[LB.Box (Just (chr (i + 48))) 0]]]
 
 
-
+-- creates a document using the non-knuth-plass, simple pagebreaking algorithm
 ownDoc :: IO ()
 ownDoc = do
   font <- defaultFont
@@ -94,9 +94,45 @@ ownDoc = do
     items = stringToItems font (TT.mobyDickChapterOne ++ TT.mobyDickChapterTwo ++ TT.mobyDickChapterThree)  defaultSpaceFactors False
   multiPageLinebreaker' items defaultLines defaultParameters font "ownDoc.dvi"
 
+-- used for debugging, prints the result of the algorithm
 examplePB :: IO()
 examplePB = do
     font <- defaultFont
     let
       items = stringToItems font (TT.mobyDickChapterOne ++ TT.mobyDickChapterTwo ++ TT.mobyDickChapterThree)  defaultSpaceFactors False
     print $ prune $ PB.knuthPlassPageBreaking (LB.knuthPlassLineBreaking' items (Just '-') defaultLines defaultParameters) defaultPageHeights defaultParameters
+
+--  first attempt to create a document using the page-breaking algorithm and the DVI-processing, printing just one page
+makePage :: Either PageBreakError [[Item [Item Char]]] -> Font -> IO()
+makePage pages font = createDVI ("./files/" ++ "something.dvi") 1000 dviUnitsTeX
+                      >>= shipOut . Page (pageNum (1 :: Int))
+                        (shiftPage (points (fromIntegral $ PB.getLeftMargin defaultMargins), points (fromIntegral $ PB.getTopMargin defaultMargins)) $ renderNode (PB.dviNodeOutput font defaultInterlineFactor (fromRight [] pages) !! 1))
+                      >>= finishDVI
+
+
+-- creates a document, using the pagebreaking algorithm
+kpbDocument :: IO()
+kpbDocument = do 
+      font <- defaultFont
+      let 
+        items = stringToItems font (TT.mobyDickChapterOne ++ TT.mobyDickChapterTwo ++ TT.mobyDickChapterThree)  defaultSpaceFactors False
+        pages = prune $ PB.knuthPlassPageBreaking (LB.knuthPlassLineBreaking' items (Just '-') defaultLines defaultParameters) defaultPageHeights defaultParameters
+      -- print pages
+      multiPageLinebreaker'' pages font "kpb_document.dvi"
+
+--  alternative version of multiPageLinebreaker' for the page-breaking algorithm instead of the simple one
+multiPageLinebreaker'' :: Either PageBreakError [[Item [Item Char]]] -> Font -> String -> IO()
+multiPageLinebreaker'' (Right pages) font fileName = do
+  createDVI ("./files/" ++ fileName) 1000 dviUnitsTeX 
+  >>= mPLhelper' 1 pages font
+  >>= finishDVI
+  where
+    mPLhelper' :: Int -> [[Item [Item Char]]] -> Font -> DocStat -> IO DocStat
+    mPLhelper' _ [] _ _= error "no valid document"
+    mPLhelper' i [p] font ds = (shipOut . Page (pageNum (i :: Int)) (concat $ pageContent' i font p)) ds
+    mPLhelper' i (p:ps) font ds | i < length defaultPageHeights = (shipOut . (Page (pageNum (i :: Int)) (concat $ pageContent' i font p))) ds 
+                                          >>= (mPLhelper' (i+1) ps font)
+                                | otherwise = (shipOut . (Page (pageNum (i :: Int)) (concat $ pageContent' i font p))) ds
+    pageContent' i font p = [shiftPage (points (fromIntegral $ PB.getLeftMargin defaultMargins), points (fromIntegral $ PB.getTopMargin defaultMargins)) $ renderNode (PB.dviNodeOutput font defaultInterlineFactor  pages !! (i-1)),
+                             shiftPage (points $ fromIntegral 226, points $ fromIntegral 30) $ renderNode $ simpleDviNodeOutput font defaultInterlineFactor [[LB.Box (Just (chr (i + 48))) 0]]]
+multiPageLinebreaker'' _ _ _ = error "no valid document"
--- src/Pagebreaking.hs
+++ src/Pagebreaking.hs
@@ -7,7 +7,8 @@ import Data.Int
 import Data.List
 import Linebreaking (knuthPlassLineBreaking)
 
-data PageBreakError = NoInput  | NoLines | NoSolutionLooseness | NoRemainingActives deriving Show
+data PageBreakError = NoInput  | NoLines | NoSolutionLooseness | NoRemainingActives
+                    | LBNoInput | LBNoLines | LBNoSolutionLooseness | LBNoRemainingActives deriving Show
 
 --  data model representing the margins of a Page (unit: pts)
 data Margins  = Margins {
@@ -65,23 +66,50 @@ simplePageBreaking (P (ls, lh)) cHeight | length ls >= lineCount = P (take lineC
         lineCount :: Int
         lineCount = floor ((65536 * fromIntegral cHeight) / lh)
 
+-- page breaking algorithm, using the knuth-plass-linebreaking algorithm to make pages out of lines
 knuthPlassPageBreaking :: Either LB.LineBreakError [[LB.Item b]] -> [LB.Width] -> LB.Parameters -> Either PageBreakError (Either LB.LineBreakError [[LB.Item [LB.Item b]]])
 knuthPlassPageBreaking (Left _) _ _ = Left NoInput
 knuthPlassPageBreaking (Right lines) pageHeights parameters'= Right (LB.knuthPlassLineBreaking' (buildLines' lines) (Just [LB.Glue 1000 100000000 100000000]) pageHeights parameters')
 
+-- makes Boxes, which the algorithm can use, out of the lines
 buildLines :: [a] -> [LB.Item a]
 buildLines  = map (\x -> LB.Box (Just x) 655360)
 
+-- adds changeable glue inbetween the lines
 buildLines' :: [b] -> [LB.Item b]
-buildLines' lines = intersperse (LB.Glue 655360 100 100) (buildLines lines)
+buildLines' lines = intersperse (LB.Glue (round $ 655360 * 1.2) 1000 1000) (buildLines lines) ++ [LB.Glue (round $ 655360 * 1.2) 1000 1000]
 
+-- reduces the complex data structure, the page-breaking algorithm gives as an output, by transforming the errors
+-- from the linebreaking into errors of the pagebreaking
 prune :: Either PageBreakError (Either LB.LineBreakError [[LB.Item [LB.Item Char]]]) -> Either PageBreakError [[LB.Item [LB.Item Char]]]
 prune (Left e) = Left e
 prune (Right (Left e)) = case e of
-                            LB.NoInput -> Left NoInput
-                            LB.NoLines -> Left NoLines
-                            LB.NoSolutionLooseness -> Left NoSolutionLooseness
-                            LB.NoRemainingActives -> Left NoRemainingActives
+                            LB.NoInput -> Left LBNoInput
+                            LB.NoLines -> Left LBNoLines
+                            LB.NoSolutionLooseness -> Left LBNoSolutionLooseness
+                            LB.NoRemainingActives -> Left LBNoRemainingActives
 prune (Right (Right xs)) = Right xs
 
+-- builds the internally used representation for Nodes, to later display the output in a DVI-File
+dviNodeOutput :: DVI.Font -> Float -> [[LB.Item [LB.Item Char]]] -> [DVI.Node]
+dviNodeOutput font ilf =  map (dviPageNodeOutput font ilf)
+    where
+        dviPageNodeOutput :: DVI.Font -> Float -> [LB.Item [LB.Item Char]] -> DVI.Node
+        dviPageNodeOutput font' ilf ((LB.Box (Just xs) bh) : (LB.Glue lh _ _):ls) =
+            DVI.wrapNode
+            $ DVI.BoxNode DVI.Vertical 0 0 0 (DVI.GlueSet 0 DVI.Fill DVI.Stretching)
+            $ DVI.interlineGlue
+             0
+            (DVI.Glue (numberConverter lh ilf) (DVI.GlueSS 0 0 0 0) (DVI.GlueSS 0 0 0 0))
+            (DVI.wrapNode $ DVI.PenaltyNode Nothing)
+            $ concatMap (packItems font') [xs : extractItems ls]
+        dviPageNodeOutput _ _ _ = error "something went wrong"
 
+        numberConverter :: Int -> Float -> Int32
+        numberConverter i f = fromIntegral $ fromEnum ((fromIntegral i :: Float) * f) :: Int32
+
+        extractItems :: [LB.Item [LB.Item Char]] -> [[LB.Item Char]]
+        extractItems [] = []
+        extractItems [LB.Box (Just xs) _] = [xs]
+        extractItems ((LB.Box (Just xs) _):ls) = xs : extractItems ls
+        extractItems (_:ls ) = extractItems ls
